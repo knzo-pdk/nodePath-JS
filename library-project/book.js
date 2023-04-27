07e639b51b999cdd18b8c0412072a830417d29ad
Kenzo Pham <khoiduc1997@gmail.com>
2023-04-26 22:55:40 -0700

    add these

--- library-project/book.js
+++ library-project/book.js
@@ -1,3 +1,20 @@
+let library = [];
+
+function addToLibrary() {
+    //do stuff
+}
+
+// // Get a reference to the button element
+window.addEventListener('load', function() {
+    const showFormButton = document.getElementById("library-add");
+    const myForm = document.getElementById("book-form");
+
+    showFormButton.addEventListener("click", function() {
+        myForm.style.display = "block";
+    });
+});
+
+
 function Book(name, author, pages, status){
     this.name = name,
     this.author = author,

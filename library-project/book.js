1d263f728dd1c7fbdfc2834a462e297ad9086dce
Kenzo Pham <khoiduc1997@gmail.com>
2023-05-07 15:36:01 -0700

    addthis

--- library-project/book.js
+++ library-project/book.js
@@ -4,29 +4,53 @@ function addToLibrary() {
     //do stuff
 }
 
-// // Get a reference to the button element
-window.addEventListener('load', function() {
-    const showFormButton = document.getElementById("library-add");
-    const myForm = document.getElementById("book-form");
-
-    showFormButton.addEventListener("click", function() {
-        myForm.style.display = "block";
-    });
-});
-
-
 function Book(name, author, pages, status){
     this.name = name,
     this.author = author,
     this.pages = pages,
     this.readOrNot = status,
-    this.info = function() {
-        var string = this.name + " by " + this.author + ", " + this.pages + " pages";
-        return string;
+    displayBook = function() {
     }
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295)
+// // Get a reference to the button element
+window.addEventListener('load', function() {
+
+    const showFormButton = document.getElementById("library-add");
+    const myForm = document.getElementById("book-form");
+    const getName = document.getElementById("name");
+    const getAuthor = document.getElementById("author");
+    const getPages = document.getElementById("pages");
+    const isRead = document.getElementById("read");
+    const showAll = document.getElementById("show-all");
+    const addBook = document.getElementById("add-book");
+
+    
+    showFormButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        myForm.style.display = "block";
+        
+    });
+
+    addBook.addEventListener("click", function(event) {
+        event.preventDefault();
+        myForm.style.display = "block";
+        const addThisBook = new Book(getName.value, getAuthor.value, getPages.value, isRead.value);
+        library.push(addThisBook);
+    });
+
+    //problem: this closes the form when the user clicks
+    showAll.addEventListener("click", function(event) {
+        event.preventDefault();
+        library.forEach(function(element) {
+            console.log(element);
+        });
+    });
+
+});
 
 
-console.log(theHobbit.info()) //"The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+
+
+
+
